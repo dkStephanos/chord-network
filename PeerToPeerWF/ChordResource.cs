db3e8f063dbcc6b0ac300b99e795dd20c1407315
dkStephanos <stephanos@goldmail.etsu.edu>
2020-10-19 15:34:09 -0400

    Added class for chord resources, contains key and filepath

---
+++ PeerToPeerWF/ChordResource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChordNodeServer
+{
+   public class ChordResource
+   {
+      public int ResourceKey { get; set; }
+      public string FilePath { get; set; }
+
+      public ChordResource(int key, string filePath)
+      {
+         ResourceKey = key;
+         FilePath = filePath;
+      }
+   }
+}
